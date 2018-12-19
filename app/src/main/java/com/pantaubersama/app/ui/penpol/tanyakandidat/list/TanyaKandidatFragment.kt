3114cdd7071fb5ed6784931f940ed517b1f819d7
Ali Mustofa <mustofa@extrainteger.com>
2018-12-19 22:44:12 +0700

    completing task #38

--- app/src/main/java/com/pantaubersama/app/ui/penpol/tanyakandidat/list/TanyaKandidatFragment.kt
+++ app/src/main/java/com/pantaubersama/app/ui/penpol/tanyakandidat/list/TanyaKandidatFragment.kt
@@ -1,10 +1,13 @@
 package com.pantaubersama.app.ui.penpol.tanyakandidat.list
 
+import android.content.Intent
 import android.support.v4.app.Fragment
 import android.view.View
 import com.pantaubersama.app.R
 import com.pantaubersama.app.base.BaseFragment
 import com.pantaubersama.app.base.BasePresenter
+import com.pantaubersama.app.ui.penpol.tanyakandidat.create.CreateTanyaKandidatActivity
+import kotlinx.android.synthetic.main.fragment_tanya_kandidat.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -17,7 +20,10 @@ class TanyaKandidatFragment : BaseFragment<BasePresenter<*>>() {
     }
 
     override fun initView(view: View) {
-//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        view.question_section.setOnClickListener {
+            val intent = Intent(context, CreateTanyaKandidatActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun setLayout(): Int {
