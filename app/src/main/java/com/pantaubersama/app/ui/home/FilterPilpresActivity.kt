9b56001338bbf3bcb02afa88149030f86d0fc990
edityomurti <edit@extrainteger.com>
2018-12-20 17:39:58 +0700

    [WIP] filter pilpres

---
+++ app/src/main/java/com/pantaubersama/app/ui/home/FilterPilpresActivity.kt
@@ -0,0 +1,12 @@
+package com.pantaubersama.app.ui.home
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FilterPilpresActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filter_pilpres)
+    }
+}
