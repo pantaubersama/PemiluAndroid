6f965afb5da3893c4226b92b6af07d93b3930ad0
edityomurti <edit@extrainteger.com>
2018-12-20 17:40:26 +0700

    [WIP] filter pilpres

--- app/src/main/java/com/pantaubersama/app/ui/home/FilterPilpresActivity.kt
+++ app/src/main/java/com/pantaubersama/app/ui/home/FilterPilpresActivity.kt
@@ -1,12 +1,37 @@
 package com.pantaubersama.app.ui.home
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.pantaubersama.app.R
+import com.pantaubersama.app.base.BaseActivity
+import com.pantaubersama.app.base.BasePresenter
 
-class FilterPilpresActivity : AppCompatActivity() {
+class FilterPilpresActivity : BaseActivity<BasePresenter<*>>() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_filter_pilpres)
+
+
+    override fun statusBarColor(): Int? {
+        return 0
+    }
+
+    override fun fetchIntentExtra() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setupUI() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setLayout(): Int {
+        return R.layout.activity_filter_pilpres
+    }
+
+    override fun showLoading() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun dismissLoading() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
