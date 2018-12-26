ef70f529d02734ffdb911bcab53d1daee97248db
Untung Suryono <untungs30@gmail.com>
2018-12-26 18:20:48 +0700

    [Penpol - Kuis] Kunci Jawaban Kuis #66

---
+++ app/src/main/java/com/pantaubersama/app/utils/OffsetItemDecoration.kt
@@ -0,0 +1,24 @@
+package com.pantaubersama.app.utils
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class OffsetItemDecoration(
+        var left: Int,
+        var top: Int = left,
+        var right: Int = left,
+        var bottom: Int = left,
+        var orientation: Int = RecyclerView.VERTICAL)
+    : RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        val isFirstItem = parent.getChildAdapterPosition(view) == 0
+
+        if (orientation == RecyclerView.VERTICAL) {
+            outRect.set(left, if (isFirstItem) top else 0, right, bottom)
+        } else {
+            outRect.set(if (isFirstItem) left else 0, top, right, bottom)
+        }
+    }
+}
