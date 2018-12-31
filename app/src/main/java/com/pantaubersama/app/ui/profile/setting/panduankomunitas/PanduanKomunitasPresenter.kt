5298b65886e33ddb5918b03c7019c3b34fc15615
yazid <yazid@aksaramaya.com>
2018-12-31 08:04:29 +0700

    panduan komunitas

---
+++ app/src/main/java/com/pantaubersama/app/ui/profile/setting/panduankomunitas/PanduanKomunitasPresenter.kt
@@ -0,0 +1,5 @@
+package com.pantaubersama.app.ui.profile.setting.panduankomunitas
+
+import com.pantaubersama.app.base.BasePresenter
+
+class PanduanKomunitasPresenter : BasePresenter<PanduangKomunitasView>()
