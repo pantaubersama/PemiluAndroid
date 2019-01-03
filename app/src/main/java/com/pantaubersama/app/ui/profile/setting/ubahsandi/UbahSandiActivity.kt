cdfc92fb1996bbbec234ad01baf053a513fea38e
Ali Mustofa <mustofa@extrainteger.com>
2019-01-03 21:15:09 +0700

    completing task #83 : ubah sandi

--- app/src/main/java/com/pantaubersama/app/ui/profile/setting/ubahsandi/UbahSandiActivity.kt
+++ app/src/main/java/com/pantaubersama/app/ui/profile/setting/ubahsandi/UbahSandiActivity.kt
@@ -1,10 +1,23 @@
 package com.pantaubersama.app.ui.profile.setting.ubahsandi
 
+import android.app.Activity
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import androidx.core.content.ContextCompat
 import com.pantaubersama.app.R
 import com.pantaubersama.app.base.BaseActivity
+import com.pantaubersama.app.base.BaseApp
+import com.pantaubersama.app.data.interactors.ProfileInteractor
+import com.pantaubersama.app.utils.ToastUtil
 import kotlinx.android.synthetic.main.activity_ubah_sandi.*
+import timber.log.Timber
+import javax.inject.Inject
 
 class UbahSandiActivity : BaseActivity<UbahSandiPresenter>(), UbahSandiView {
+    @Inject
+    lateinit var profileInteractor: ProfileInteractor
+    var passwordValid = false
 
     override fun statusBarColor(): Int? {
         return 0
@@ -14,13 +27,51 @@ class UbahSandiActivity : BaseActivity<UbahSandiPresenter>(), UbahSandiView {
         // ok
     }
 
+    override fun initInjection() {
+        (application as BaseApp).createActivityComponent(this)?.inject(this)
+    }
+
     override fun initPresenter(): UbahSandiPresenter? {
-        return UbahSandiPresenter()
+        return UbahSandiPresenter(profileInteractor)
     }
 
     override fun setupUI() {
         setupToolbar(true, getString(R.string.title_ubah_sandi), R.color.white, 4f)
+        setupPasswordField()
         onClickAction()
+    }
+
+    private fun setupPasswordField() {
+        ubah_sandi_sandi_baru.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onTextChanged(string: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (string?.length!! < 8) {
+                    ubah_sandi_sandi_baru.error = "Password minimal 8 karakter"
+                }
+            }
+        })
+        repeat_password.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onTextChanged(string: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (string.toString() != ubah_sandi_sandi_baru.text.toString()) {
+                    repeat_password.error = "Password tidak cocok"
+                }
+            }
+        })
     }
 
     override fun setLayout(): Int {
@@ -28,16 +79,58 @@ class UbahSandiActivity : BaseActivity<UbahSandiPresenter>(), UbahSandiView {
     }
 
     override fun showLoading() {
-        // show loading
+        loading.visibility = View.VISIBLE
     }
 
     override fun dismissLoading() {
-        // hide loading
+        loading.visibility = View.VISIBLE
     }
 
     private fun onClickAction() {
         ubah_sandi_ubah.setOnClickListener {
-            // ubah password
+            if (ubah_sandi_sandi_baru.error == null) {
+                if (repeat_password.error == null) {
+                    if (ubah_sandi_lama.text.isNotEmpty()) {
+                        presenter?.updatePassword(
+                            ubah_sandi_sandi_baru.text.toString(),
+                            repeat_password.text.toString()
+                        )
+                    } else {
+                        ubah_sandi_lama.error = "Passowrd lama harus diisi"
+                    }
+                }
+            }
+        }
+    }
+
+    override fun showSuccessUpdatePasswordAlert() {
+        ToastUtil.show(this@UbahSandiActivity, "Berhasil memperbarui password")
+    }
+
+    override fun finishActivity() {
+        setResult(Activity.RESULT_OK)
+        finish()
+    }
+
+    override fun showFailedUpdatePasswordAlert() {
+        ToastUtil.show(this@UbahSandiActivity, "Gagal memperbarui password")
+    }
+
+    override fun disableView() {
+        ubah_sandi_ubah.isEnabled = false
+        ubah_sandi_ubah.setBackgroundColor(ContextCompat.getColor(this@UbahSandiActivity, R.color.gray_dark_1))
+        for (i in 0 until ubah_password_container.childCount) {
+            val child = ubah_password_container.getChildAt(i)
+            child.isEnabled = false
+        }
+    }
+
+    override fun enableView() {
+        ubah_sandi_ubah.isEnabled = true
+        ubah_sandi_ubah.setBackgroundColor(ContextCompat.getColor(this@UbahSandiActivity, R.color.colorPrimary))
+        for (i in 0 until ubah_password_container.childCount) {
+            val child = ubah_password_container.getChildAt(i)
+            child.isEnabled = true
         }
     }
 }
