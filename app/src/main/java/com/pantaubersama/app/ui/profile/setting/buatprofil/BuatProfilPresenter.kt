449230700124dc50477ba3b22b5c89ae12b6e0f6
yazid <yazid@aksaramaya.com>
2019-01-10 22:04:18 +0700

    layout buat profil

---
+++ app/src/main/java/com/pantaubersama/app/ui/profile/setting/buatprofil/BuatProfilPresenter.kt
@@ -0,0 +1,6 @@
+package com.pantaubersama.app.ui.profile.setting.buatprofil
+
+import com.pantaubersama.app.base.BasePresenter
+import com.pantaubersama.app.base.BaseView
+
+class BuatProfilPresenter : BasePresenter<BaseView>()
