c9db08e6cf30b2b19c9af78224bdfaf89cf3d1dd
Ali Mustofa <mustofa@extrainteger.com>
2019-01-15 17:42:58 +0700

    completing task #91 & task #100 : Ada sedikit problem setelah login twitter gagal memuat data pengguna karena component belum re-init

--- app/src/main/java/com/pantaubersama/app/ui/profile/connect/ConnectPresenter.kt
+++ app/src/main/java/com/pantaubersama/app/ui/profile/connect/ConnectPresenter.kt
@@ -2,6 +2,11 @@ package com.pantaubersama.app.ui.profile.connect
 
 import com.pantaubersama.app.base.BasePresenter
 import com.pantaubersama.app.data.interactors.LoginInteractor
+import com.pantaubersama.app.utils.PantauConstants
+import com.twitter.sdk.android.core.Callback
+import com.twitter.sdk.android.core.Result
+import com.twitter.sdk.android.core.TwitterException
+import com.twitter.sdk.android.core.models.User
 import javax.inject.Inject
 
 class ConnectPresenter @Inject constructor(private val loginInteractor: LoginInteractor) : BasePresenter<ConnectView>() {
@@ -42,6 +47,49 @@ class ConnectPresenter @Inject constructor(private val loginInteractor: LoginInt
                         view?.dismissLoading()
                         view?.showError(it)
                         view?.showFailedToConnectTwitterAlert()
+                    }
+                )
+        )
+    }
+
+    fun getTwitterUserData() {
+        loginInteractor.getTwitterAccountService()
+            .verifyCredentials(false, false, false)
+            .enqueue(object : Callback<User>() {
+                override fun success(result: Result<User>?) {
+                    view?.bindTwitterUserData(result?.data)
+                }
+
+                override fun failure(exception: TwitterException?) {
+                    view?.showError(exception as Throwable)
+                    view?.showFailedGetUserDataAlert()
+                }
+            })
+    }
+
+    fun disconnectSocialMedia(accountType: String) {
+        view?.showLoading()
+        disposables.add(
+            loginInteractor.disconnectSocielMedia(accountType)
+                .subscribe(
+                    {
+                        view?.dismissLoading()
+                        if (accountType == PantauConstants.CONNECT.FACEBOOK) {
+                            view?.showSuccessDisconnectFacebookAlert()
+                            view?.logoutFacebookSDK()
+                        } else if (accountType == PantauConstants.CONNECT.TWITTER) {
+                            view?.showSuccessDisconnectTwitterAlert()
+                            view?.logoutTwitterSDK()
+                        }
+                    },
+                    {
+                        view?.dismissLoading()
+                        view?.showError(it)
+                        if (accountType == PantauConstants.CONNECT.FACEBOOK) {
+                            view?.showFailedDisconnectFacebookAlert()
+                        } else if (accountType == PantauConstants.CONNECT.TWITTER) {
+                            view?.showFailedDisconnectTwitterAlert()
+                        }
                     }
                 )
         )
