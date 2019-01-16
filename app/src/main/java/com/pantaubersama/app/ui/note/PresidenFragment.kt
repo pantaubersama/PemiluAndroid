78f399b5d040c4d012dab66b217c2b90e3065083
Ali Mustofa <mustofa@extrainteger.com>
2019-01-16 16:52:19 +0700

    WIP task #112 : Catatan pilihanku

--- app/src/main/java/com/pantaubersama/app/ui/note/PresidenFragment.kt
+++ app/src/main/java/com/pantaubersama/app/ui/note/PresidenFragment.kt
@@ -1,21 +1,52 @@
 package com.pantaubersama.app.ui.note
 
 import android.view.View
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.pantaubersama.app.R
 import com.pantaubersama.app.base.CommonFragment
+import com.pantaubersama.app.data.model.capres.PaslonData
+import kotlinx.android.synthetic.main.fragment_presiden.view.*
 
 class PresidenFragment : CommonFragment() {
+    private lateinit var layoutManager: StaggeredGridLayoutManager
+    private lateinit var paslonAdapter: PaslonAdapter
 
     override fun initView(view: View) {
-        setupRecyclerView()
+        setupRecyclerView(view)
+        setData()
     }
 
-    private fun setupRecyclerView() {
-        /*
-        RecyclerView untuk menampilkan calon presiden
-        style menggunakan StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-        item layoutnya menggunakan item_pilihan.xml
-         */
+    private fun setData() {
+        val paslons: MutableList<PaslonData> = ArrayList()
+        paslons.add(
+            PaslonData(
+                1,
+                "Jokowi - Ma'ruf",
+                R.drawable.ava_calon_1,
+                "Joko Widodo",
+                "Ma'ruf Amin")
+        )
+        paslons.add(
+            PaslonData(
+                2,
+                "Prabowo - Sandi",
+                R.drawable.ava_calon_2,
+                "Prabowo Subiyanto",
+                "Sandiaga Salahudin Uno")
+        )
+        paslons.add(
+            PaslonData(
+                3,
+                "Belum menentukan pilihan")
+        )
+        paslonAdapter.setDatas(paslons)
+    }
+
+    private fun setupRecyclerView(view: View) {
+        layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        paslonAdapter = PaslonAdapter()
+        view.presiden_recycler_view_calon.layoutManager = layoutManager
+        view.presiden_recycler_view_calon.adapter = paslonAdapter
     }
 
     override fun setLayout(): Int {
