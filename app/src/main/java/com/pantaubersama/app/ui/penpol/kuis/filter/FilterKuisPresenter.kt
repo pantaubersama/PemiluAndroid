cdff64c292c9a0c5f974bd940f475d6383c74839
Ali Mustofa <mustofa@extrainteger.com>
2019-01-18 17:20:32 +0700

    hide kuis filter option when no login

--- app/src/main/java/com/pantaubersama/app/ui/penpol/kuis/filter/FilterKuisPresenter.kt
+++ app/src/main/java/com/pantaubersama/app/ui/penpol/kuis/filter/FilterKuisPresenter.kt
@@ -2,15 +2,19 @@ package com.pantaubersama.app.ui.penpol.kuis.filter
 
 import com.pantaubersama.app.base.BasePresenter
 import com.pantaubersama.app.data.interactors.KuisInteractor
+import com.pantaubersama.app.data.interactors.ProfileInteractor
 import javax.inject.Inject
 
-class FilterKuisPresenter @Inject constructor(private val kuisInteractor: KuisInteractor) : BasePresenter<FilterKuisView>() {
+class FilterKuisPresenter @Inject constructor(
+    private val kuisInteractor: KuisInteractor,
+    private val profileInteractor: ProfileInteractor
+) : BasePresenter<FilterKuisView>() {
     fun getKuisFilterData() {
         view?.setKuisFilter(kuisInteractor.getKuisFilter())
     }
 
     fun saveKuisFilter(kuisFilter: String) {
-        disposables?.add(
+        disposables.add(
             kuisInteractor
                 .saveKuisFilter(kuisFilter)
                 .subscribe(
@@ -24,5 +28,9 @@ class FilterKuisPresenter @Inject constructor(private val kuisInteractor: KuisIn
                     }
                 )
         )
+    }
+
+    fun getProfile() {
+        view?.bindProfile(profileInteractor.getProfile())
     }
 }
