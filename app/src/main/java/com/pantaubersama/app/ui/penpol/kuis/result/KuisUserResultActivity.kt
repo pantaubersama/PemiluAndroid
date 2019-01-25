289a69a77b14b1af50d6cd7d3919d3cb17d803ab
edityomurti <edit@extrainteger.com>
2019-01-25 22:03:15 +0700

    [WIP] share as screenshot hasil kuis

--- app/src/main/java/com/pantaubersama/app/ui/penpol/kuis/result/KuisUserResultActivity.kt
+++ app/src/main/java/com/pantaubersama/app/ui/penpol/kuis/result/KuisUserResultActivity.kt
@@ -1,7 +1,11 @@
 package com.pantaubersama.app.ui.penpol.kuis.result
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
 import com.facebook.internal.NativeProtocol.EXTRA_USER_ID
@@ -10,12 +14,15 @@ import com.pantaubersama.app.base.BaseActivity
 import com.pantaubersama.app.data.model.kuis.KuisUserResult
 import com.pantaubersama.app.di.component.ActivityComponent
 import com.pantaubersama.app.ui.home.HomeActivity
+import com.pantaubersama.app.utils.ImageUtil
 import com.pantaubersama.app.utils.ShareUtil
+import com.pantaubersama.app.utils.ToastUtil
 import com.pantaubersama.app.utils.extensions.color
 import com.pantaubersama.app.utils.extensions.loadUrl
 import com.pantaubersama.app.utils.extensions.visibleIf
 import com.pantaubersama.app.utils.spannable
 import kotlinx.android.synthetic.main.activity_kuis_user_result.*
+import java.io.File
 import javax.inject.Inject
 import kotlin.math.roundToInt
 
@@ -78,7 +85,41 @@ class KuisUserResultActivity : BaseActivity<KuisUserResultPresenter>(), KuisUser
         tv_percentage.text = "%d%%".format(kuisUserResult.percentage.roundToInt())
         tv_paslon_name.text = kuisUserResult.team.title
         btn_share.setOnClickListener {
-            ShareUtil.shareItem(this, kuisUserResult)
+//            ShareUtil.shareItem(this, kuisUserResult)
+            takeScreenShot()
+        }
+    }
+
+    private fun takeScreenShot() {
+        showProgressDialog("Tunggu yakk ...")
+        val rootView = window.decorView.rootView
+        val bitmap = Bitmap.createBitmap(rootView.width, rootView.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        setupToolbar(false, "", R.color.white, 0f)
+        rootView.draw(canvas)
+        shareOnWhatsApp(ImageUtil.getImageFile(bitmap))
+    }
+
+    private fun shareOnWhatsApp(imageFile: File) {
+        val imageUri = Uri.parse(imageFile.absolutePath)
+        val shareIntent = Intent()
+        shareIntent.setAction(Intent.ACTION_SEND)
+
+//        shareIntent.setPackage("com.whatsapp")
+//        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "eh iso")
+        shareIntent.putExtra(Intent.EXTRA_STREAM, imageUri)
+        shareIntent.setType("*/*")
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        shareIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+        setupToolbar(true, "", R.color.white, 0f)
+        dismissProgressDialog()
+
+        try {
+            startActivity(shareIntent)
+        } catch (e: ActivityNotFoundException) {
+            ToastUtil.show(this, "Oops.. ada yang salah nih")
         }
     }
 
