75ce2d841471de974aa8ea48d4aebbfb3b46062e
ntyazid <ntyazid@gmail.com>
2019-02-08 09:41:53 +0700

    layout buat tantangan

---
+++ app/src/main/java/com/pantaubersama/app/ui/wordstadium/challenge/CreateChallengePresenter.kt
@@ -0,0 +1,5 @@
+package com.pantaubersama.app.ui.wordstadium.challenge
+
+import com.pantaubersama.app.base.BasePresenter
+
+class CreateChallengePresenter : BasePresenter<CreateChallengeView>()
---
+++ app/src/main/java/com/pantaubersama/app/ui/wordstadium/challenge/CreateChallengeView.kt
@@ -0,0 +1,5 @@
+package com.pantaubersama.app.ui.wordstadium.challenge
+
+import com.pantaubersama.app.base.BaseView
+
+interface CreateChallengeView : BaseView
