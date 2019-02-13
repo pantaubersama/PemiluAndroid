94a9a3a3c9c87c33f12b879cd89b1b7ae3fd42dd
Ali Mustofa <mustofa@extrainteger.com>
2019-02-13 16:54:13 +0700

    layout create data tps

--- app/src/main/java/com/pantaubersama/app/ui/merayakan/perhitungan/create/datatps/DataTPSActivity.kt
+++ app/src/main/java/com/pantaubersama/app/ui/merayakan/perhitungan/create/datatps/DataTPSActivity.kt
@@ -1,19 +1,28 @@
 package com.pantaubersama.app.ui.merayakan.perhitungan.create.datatps
 
+import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.location.* //ktlint-disable
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import com.pantaubersama.app.CommonActivity
 import com.pantaubersama.app.R
+import com.pantaubersama.app.utils.PantauConstants.RequestCode.RC_ASK_PERMISSIONS
+import com.pantaubersama.app.utils.ToastUtil
+import kotlinx.android.synthetic.main.activity_data_tps.*
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
+import pub.devrel.easypermissions.PermissionRequest
+import java.util.Locale
 
-class DataTPSActivity : CommonActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+class DataTPSActivity : CommonActivity(), LocationListener {
+    var locationManager: LocationManager? = null
+    private var permission = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+    private var geocoder: Geocoder? = null
+    private var addresses: List<Address>? = null
 
     override fun statusBarColor(): Int? {
         return 0
@@ -25,6 +34,49 @@ class DataTPSActivity : CommonActivity() {
 
     override fun setupUI(savedInstanceState: Bundle?) {
         setupToolbar(true, "Data TPS", R.color.white, 4f)
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        geocoder = Geocoder(this, Locale.getDefault())
+        getLocation()
+    }
+
+    @AfterPermissionGranted(RC_ASK_PERMISSIONS)
+    private fun getLocation() {
+        if (EasyPermissions.hasPermissions(this, *permission)) {
+            try {
+                locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, this)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        } else {
+            EasyPermissions.requestPermissions(
+                PermissionRequest.Builder(this, RC_ASK_PERMISSIONS, *permission)
+                    .setRationale(getString(R.string.izinkan_akses_lokasi))
+                    .setPositiveButtonText(getString(R.string.ok))
+                    .setNegativeButtonText(getString(R.string.batal))
+                    .build()
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onLocationChanged(location: Location) {
+        addresses = geocoder?.getFromLocation(location.latitude, location.longitude, 1)
+        val address = addresses?.get(0)?.getAddressLine(0)
+        address_text.text = address
+    }
+
+    override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+    }
+
+    override fun onProviderEnabled(p0: String?) {
+    }
+
+    override fun onProviderDisabled(p0: String?) {
+        ToastUtil.show(this@DataTPSActivity, "Hidupkan GPS kamu")
     }
 
     companion object {
