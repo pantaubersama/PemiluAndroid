f6a257242c6edb14db83623d22225ea2e867f111
Ali Mustofa <mustofa@extrainteger.com>
2019-02-14 18:07:29 +0700

    fixing layout

--- app/src/main/java/com/pantaubersama/app/ui/notification/NotifAdapter.kt
+++ app/src/main/java/com/pantaubersama/app/ui/notification/NotifAdapter.kt
@@ -8,6 +8,7 @@ import com.pantaubersama.app.base.BaseRecyclerAdapter
 import com.pantaubersama.app.data.model.notification.NotificationWhole
 import com.pantaubersama.app.utils.extensions.inflate
 import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_notif_badge.*
 import kotlinx.android.synthetic.main.item_notif_general.*
 import kotlinx.android.synthetic.main.item_notif_new_quiz.*
 import kotlinx.android.synthetic.main.item_notif_question.*
@@ -18,6 +19,7 @@ class NotifAdapter : BaseRecyclerAdapter() {
         const val NOTIF_TYPE_GENERAL = 0
         const val NOTIF_TYPE_QUESTION = 1
         const val NOTIF_TYPE_QUIZ = 2
+        const val NOTIF_TYPE_BADGE = 3
     }
 
     override fun getItemViewType(position: Int): Int {
@@ -25,11 +27,13 @@ class NotifAdapter : BaseRecyclerAdapter() {
             (data[position] as NotificationWhole).data.notif_type != null -> when ((data[position] as NotificationWhole).data.notif_type) {
                 "quiz" -> NOTIF_TYPE_QUIZ
                 "question" -> NOTIF_TYPE_QUESTION
+                "badge" -> NOTIF_TYPE_BADGE
                 else -> NOTIF_TYPE_GENERAL
             }
             (data[position] as NotificationWhole).data.payload.notif_type != null -> when ((data[position] as NotificationWhole).data.payload.notif_type) {
                 "quiz" -> NOTIF_TYPE_QUIZ
                 "question" -> NOTIF_TYPE_QUESTION
+                "badge" -> NOTIF_TYPE_BADGE
                 else -> NOTIF_TYPE_GENERAL
             }
             else -> NOTIF_TYPE_GENERAL
@@ -40,6 +44,7 @@ class NotifAdapter : BaseRecyclerAdapter() {
         return when (viewType) {
             NOTIF_TYPE_QUIZ -> QuizViewHolder(parent.inflate(R.layout.item_notif_new_quiz))
             NOTIF_TYPE_QUESTION -> QuestionVideHolder(parent.inflate(R.layout.item_notif_question))
+            NOTIF_TYPE_BADGE -> BadgeViewHolder(parent.inflate(R.layout.item_notif_badge))
             else -> NotificationViewHolder(parent.inflate(R.layout.item_notif_general))
         }
     }
@@ -47,6 +52,7 @@ class NotifAdapter : BaseRecyclerAdapter() {
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         (holder as? QuizViewHolder)?.bind(data[position] as NotificationWhole)
         (holder as? QuestionVideHolder)?.bind(data[position] as NotificationWhole)
+        (holder as? BadgeViewHolder)?.bind(data[position] as NotificationWhole)
         (holder as? NotificationViewHolder)?.bind(data[position] as NotificationWhole)
     }
 
@@ -66,6 +72,12 @@ class NotifAdapter : BaseRecyclerAdapter() {
         fun bind(notification: NotificationWhole) {
             notif_question_message.text = notification.notification.body
             notif_question_text.text = notification.data.payload.question.body
+        }
+    }
+
+    inner class BadgeViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+        fun bind(notification: NotificationWhole) {
+            notif_badge_message.text = notification.notification.body
         }
     }
 }
