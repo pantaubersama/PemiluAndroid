c6cfedf6c5bc7b0e20c4cff4fadc460edadf4732
Ali Mustofa <mustofa@extrainteger.com>
2019-02-21 19:38:46 +0700

    task #82 onprogress

---
+++ app/src/main/java/com/pantaubersama/app/ui/penpol/tanyakandidat/create/QuestionsAvailableAdapter.kt
@@ -0,0 +1,36 @@
+package com.pantaubersama.app.ui.penpol.tanyakandidat.create
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.pantaubersama.app.R
+import com.pantaubersama.app.base.BaseRecyclerAdapter
+import com.pantaubersama.app.data.model.tanyakandidat.Pertanyaan
+import com.pantaubersama.app.utils.extensions.inflate
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.question_suggestion_item.*
+
+class QuestionsAvailableAdapter : BaseRecyclerAdapter() {
+    var listener: Listener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return QuestionsAvailableViewHolder(parent.inflate(R.layout.question_suggestion_item))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as QuestionsAvailableViewHolder).bind(data[position] as Pertanyaan)
+    }
+
+    inner class QuestionsAvailableViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+        fun bind(item: Pertanyaan) {
+            question.text = item.body
+            itemView.setOnClickListener {
+                listener?.onClickQuestion(item)
+            }
+        }
+    }
+
+    interface Listener {
+        fun onClickQuestion(item: Pertanyaan)
+    }
+}
