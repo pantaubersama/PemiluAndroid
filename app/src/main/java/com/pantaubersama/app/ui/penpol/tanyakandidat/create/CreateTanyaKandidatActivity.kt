966137265863bd440a470e932059352f58a879dc
Ali Mustofa <mustofa@extrainteger.com>
2019-02-21 19:38:46 +0700

    task #82 onprogress

--- app/src/main/java/com/pantaubersama/app/ui/penpol/tanyakandidat/create/CreateTanyaKandidatActivity.kt
+++ app/src/main/java/com/pantaubersama/app/ui/penpol/tanyakandidat/create/CreateTanyaKandidatActivity.kt
@@ -8,15 +8,23 @@ import android.text.TextWatcher
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.jakewharton.rxbinding2.widget.RxTextView
 import com.pantaubersama.app.R
 import com.pantaubersama.app.base.BaseActivity
+import com.pantaubersama.app.data.model.ItemModel
 import com.pantaubersama.app.data.model.tanyakandidat.Pertanyaan
 import com.pantaubersama.app.data.model.user.Profile
 import com.pantaubersama.app.di.component.ActivityComponent
 import com.pantaubersama.app.utils.PantauConstants
 import com.pantaubersama.app.utils.ToastUtil
 import com.pantaubersama.app.utils.extensions.loadUrl
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_create_tanya_kandidat.*
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class CreateTanyaKandidatActivity : BaseActivity<CreateTanyaKandidatPresenter>(), CreateTanyaKandidatView {
@@ -25,6 +33,8 @@ class CreateTanyaKandidatActivity : BaseActivity<CreateTanyaKandidatPresenter>()
     override lateinit var presenter: CreateTanyaKandidatPresenter
 
     private var isLoading = false
+    private lateinit var adapter: QuestionsAvailableAdapter
+    private lateinit var disposables: CompositeDisposable
 
     override fun statusBarColor(): Int {
         return 0
@@ -54,6 +64,40 @@ class CreateTanyaKandidatActivity : BaseActivity<CreateTanyaKandidatPresenter>()
                 question_length.text = string?.length.toString()
             }
         })
+        setupAvailableQuestions()
+        disposables = CompositeDisposable()
+        RxTextView.textChanges(question)
+            .debounce(1000, TimeUnit.MILLISECONDS)
+            .map { it.toString() }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                if (it.isNotEmpty()) {
+                    adapter.setDataEnd(false)
+                    presenter.getAvailableQuestions(it)
+                } else {
+                    available_questions_container.visibility = View.GONE
+                }
+            }.addTo(disposables)
+    }
+
+    private fun setupAvailableQuestions() {
+        adapter = QuestionsAvailableAdapter()
+        available_questions.layoutManager = LinearLayoutManager(this@CreateTanyaKandidatActivity)
+        available_questions.adapter = adapter
+    }
+
+    override fun bindAvailableQuestions(questions: MutableList<Pertanyaan>) {
+        available_questions_container.visibility = View.VISIBLE
+        adapter.setDatas(questions as MutableList<ItemModel>)
+    }
+
+    override fun showFailedGetAvailableQuestions() {
+        ToastUtil.show(this@CreateTanyaKandidatActivity, "Gagal memuat pertanyaan tersedia")
+    }
+
+    override fun showEmptyAvailableQuestionAlert() {
+        ToastUtil.show(this@CreateTanyaKandidatActivity, "Tidak terdapat pertanyaan tersedia")
     }
 
     override fun setLayout(): Int {
@@ -113,5 +157,18 @@ class CreateTanyaKandidatActivity : BaseActivity<CreateTanyaKandidatPresenter>()
     override fun hideActions() {
         isLoading = true
         invalidateOptionsMenu()
+    }
+
+    override fun onBackPressed() {
+        if (available_questions_container.visibility == View.VISIBLE) {
+            available_questions_container.visibility = View.GONE
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onDestroy() {
+        disposables.clear()
+        super.onDestroy()
     }
 }
