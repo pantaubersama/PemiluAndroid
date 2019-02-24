2764596765bb4cff93b6ed7e7e386e4b41f6cf7c
Untung Suryono <untungs30@gmail.com>
2019-02-24 20:03:28 +0700

    show option dialog when pressing button more on Debat item

--- app/src/main/java/com/pantaubersama/app/ui/menguji/adapter/DebatSmallViewHolder.kt
+++ app/src/main/java/com/pantaubersama/app/ui/menguji/adapter/DebatSmallViewHolder.kt
@@ -3,13 +3,14 @@ package com.pantaubersama.app.ui.menguji.adapter
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.fragment.app.FragmentManager
 import com.pantaubersama.app.R
 import com.pantaubersama.app.data.model.debat.DebatItem
 import com.pantaubersama.app.utils.extensions.color
 import com.pantaubersama.app.utils.extensions.visibleIf
 import kotlinx.android.synthetic.main.item_debat_small.*
 
-class DebatSmallViewHolder(view: View) : DebatViewHolder(view) {
+class DebatSmallViewHolder(view: View, fm: FragmentManager) : DebatViewHolder(view, fm) {
 
     override val textDebatType: TextView = text_debat_type
     override val imageAvatar1: ImageView = image_avatar_1
@@ -18,6 +19,7 @@ class DebatSmallViewHolder(view: View) : DebatViewHolder(view) {
     override val textName2: TextView = text_name_2
     override val textTag: TextView = text_tag
     override val textOpponentCount: TextView = text_opponent_count
+    override val buttonMoreOption: View = button_more
 
     override fun bind(item: DebatItem) {
         super.bind(item)
