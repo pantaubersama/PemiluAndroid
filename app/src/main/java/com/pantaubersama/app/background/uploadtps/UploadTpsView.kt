44008c984475713f58740684369a75736c148317
Ali Mustofa <mustofa@extrainteger.com>
2019-03-25 13:20:13 +0700

    fixing

--- app/src/main/java/com/pantaubersama/app/background/uploadtps/UploadTpsView.kt
+++ app/src/main/java/com/pantaubersama/app/background/uploadtps/UploadTpsView.kt
@@ -3,6 +3,6 @@ package com.pantaubersama.app.background.uploadtps
 interface UploadTpsView {
     fun increaseProgress(progress: Int)
     fun showError(throwable: Throwable)
-    fun showFailed()
+    fun showFailed(message: String?)
     fun onSuccessPublishTps()
 }
