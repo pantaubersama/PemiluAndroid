183f32aa6b40e97e0f2f6fd1dced7b868c196626
edityo <swakresnaedityo@gmail.com>
2019-04-10 20:42:49 +0700

    Add DateExtensions

---
+++ app/src/main/java/com/pantaubersama/app/utils/extensions/DateExtensions.kt
@@ -0,0 +1,20 @@
+package com.pantaubersama.app.utils.extensions
+
+import android.content.Context
+import java.util.*
+
+/**
+ * @author edityomurti on 10/04/2019 20:32
+ */
+
+fun Context.isVoteDay(): Boolean {
+    return Calendar.getInstance().timeInMillis <= 1555434000000 // 1555434000000 == Wed Apr 17 2019 00:00:00 GMT+7
+}
+
+fun String.isDueTime(timeInMillis: Long): Boolean {
+    return Calendar.getInstance().timeInMillis <= timeInMillis
+}
+
+fun Context.getVoteDay(): Long {
+    return 1555434000000
+}
